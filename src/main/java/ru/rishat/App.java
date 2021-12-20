f6e6e29f9fbefd54bdf80811ebe02e19d578f904
Rishat Suleymanov <spk_irbis@mail.ru>
2021-12-20 08:36:04 +0300

    Initial commit

---
+++ src/main/java/ru/rishat/App.java
@@ -0,0 +1,8 @@
+package ru.rishat;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println(2 << 1);
+    }
+}
---
+++ src/test/java/ru/rishat/AppTest.java
@@ -0,0 +1,14 @@
+package ru.rishat;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+    @Test
+    public void bool() {
+        assertTrue(true);
+    }
+
+}
